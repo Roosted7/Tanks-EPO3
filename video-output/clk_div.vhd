edf775713adc74fee64be2760145f650cc3e5fba
tijs.moree <t.moree@student.tudelft.nl>
2016-11-28 15:49:20 +0100

    try to make hsync signal in clk_div

--- video-output/clk_div.vhd
+++ video-output/clk_div.vhd
@@ -5,48 +5,50 @@ use ieee.numeric_std.all;
 entity clk_div is
 	port(
 		clk, reset:					in  std_logic;
-		blck, row, frame, vbullet: 	out std_logic);
+		pixel, row, frame, vbullet, hsync, vsync: 	out std_logic);
 end clk_div;
 
 architecture arch of clk_div is
-signal cnt_blck: 	std_logic_vector(2 downto 0) := (others => '0');
-signal cnt_row:		std_logic_vector(4 downto 0) := (others => '0');
-signal cnt_frame:	std_logic_vector(8 downto 0) := (others => '0');
+signal cnt_pixel: 	std_logic_vector(2 downto 0) := (others => '0');
+signal cnt_row:		std_logic_vector(5 downto 0) := (others => '0');
+signal cnt_frame:	std_logic_vector(9 downto 0) := (others => '0');
 
-signal blck_i: 		std_logic := '1';
+signal pixel_i: 	std_logic := '1';
 signal row_i:		std_logic := '1';
 signal frame_i:		std_logic := '1';
 signal vbullet_i:	std_logic := '1';
 
 begin
-	process(reset, clk) -- create block clock
+	process(reset, clk) -- create pixel clock
 	begin
 		if(reset = '1') then
-			cnt_blck <= (others => '0');
-			blck_i <= '1';
+			cnt_pixel <= (others => '0');
+			pixel_i <= '1';
 		elsif(rising_edge(clk)) then
-			cnt_blck <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_blck)) + 1, 3));
+			cnt_pixel <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_pixel)) + 1, 3));
 			
-			if(to_integer(unsigned(cnt_blck)) = 4) then -- 6 = 5 - 1; 5 is from clock frequency to the block frequency
-				blck_i <= '1';
-				cnt_blck <= (others => '0');
-			elsif(to_integer(unsigned(cnt_blck)) = 2) then -- 2 = 4 / 2
-				blck_i <= '0';
+			if(to_integer(unsigned(cnt_pixel)) = 4) then -- 4 = 5 - 1; 5 is from clock frequency to the pixel frequency
+				pixel_i <= '1';
+				cnt_pixel <= (others => '0');
+			elsif(to_integer(unsigned(cnt_pixel)) = 2) then -- 2 = 4 / 2
+				pixel_i <= '0';
 			end if;
 		end if;
 	end process;
 
-	process(reset, blck_i) -- create row clock
+	process(reset, pixel_i) -- create row clock
 	begin
 		if(reset = '1') then
 			cnt_row <= (others => '0');
 			row_i <= '1';
-		elsif(rising_edge(blck_i)) then
-			if(to_integer(unsigned(cnt_row)) = 17) then -- 17 = 36 / 2 - 1; 36 = 29 + hsync
-				row_i <= not(row_i);
+		elsif(rising_edge(pixel_i)) then
+			cnt_row <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_row)) + 1, 6));
+
+			if(to_integer(unsigned(cnt_row)) = 35) then -- 35 = 36 - 1; 36 = 29 + hsync
+				row_i <= '1';
 				cnt_row <= (others => '0');
-			else
-				cnt_row <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_row)) + 1, 5));
+			elsif(to_integer(unsigned(cnt_row)) = 17) then -- 17 = 36 / 2 - 1
+				row_i <= '0';
 			end if;
 		end if;
 	end process;
@@ -77,7 +79,20 @@ begin
 		end if;
 	end process;
 
-	blck <= blck_i;
+	process(reset, row_i) -- create hsync signal
+	begin
+		if(reset = '1') then
+			hsync <= '0';
+		elsif(rising_edge(row_i)) then
+			if((to_integer(unsigned(cnt_pixel)) > 30) and (to_integer(unsigned(cnt_pixel)) < 35)) then
+				hsync <= '0';
+			else
+				hsync <= '1';
+			end if;
+		end if;
+	end process;
+
+	pixel <= pixel_i;
 	row <= row_i;
 	frame <= frame_i;
 	vbullet <= vbullet_i;
