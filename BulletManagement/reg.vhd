6358bce44590c28bc102b7e9b79b18e9486423da
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2016-11-28 19:30:41 +0100

    Update reg.vhd

--- BulletManagement/reg.vhd
+++ BulletManagement/reg.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 entity reg_11 is
   port( clk     : in std_logic;
         reset   : in std_logic;
-        d       : in std_logic_vector(10 downto 0)
+        d       : in std_logic_vector(10 downto 0);
         q       : out std_logic_vector(10 downto 0));
 end reg_11;
 
@@ -14,9 +14,9 @@ begin
   
   process (clk) is
   begin
-    if(rising_edge(clk) then
+    if(rising_edge(clk)) then
        if (reset = '1') then
-          q <= '00000000000';
+          q <= (others => '0');
        else
           q <= d;
        end if;
