88365e4bb497bcdb67bfececa26e97ecc7943f7e
tijs.moree <t.moree@student.tudelft.nl>
2016-12-02 23:00:57 +0100

    alleen cnt_y werkt nog nie, volgens mij

--- video-output/timebase.vhd
+++ video-output/timebase.vhd
@@ -6,7 +6,8 @@ entity timebase is
 	port(
 		clk, reset:						in  std_logic;
 		pixel, frame, vbullet:			out std_logic;
-		hsync, vsync, x_c, y_c: 		out std_logic);
+		hsync, vsync: 					out std_logic;
+		x, y:							out std_logic_vector(3 downto 0));
 end timebase;
 
 architecture arch of timebase is
@@ -14,15 +15,13 @@ architecture arch of timebase is
 	signal cnt_row:			std_logic_vector(4 downto 0);
 	signal cnt_frame:		std_logic_vector(9 downto 0);
 	signal cnt_yblck:		std_logic_vector(4 downto 0);
+	
+	signal cnt_x, cnt_y: 	std_logic_vector(3 downto 0);
 
-	signal x_i, y_i:		std_logic;
 	signal pixel_i:			std_logic;
 	signal row_i:			std_logic;
 	signal frame_i:			std_logic;
 	signal vbullet_i:		std_logic;
-
-	type pixel_type is (p0, p1);
-	signal pixel_curr, pixel_next: pixel_type;
 
 begin
 	process(reset, clk)
@@ -32,29 +31,76 @@ begin
 				cnt_pixel 	<= (others => '0');
 				cnt_row 	<= (others => '0');
 				cnt_frame 	<= (others => '0');
+				--vo
+				cnt_x 		<= (others => '0');
+				cnt_y 		<= (others => '0');
 				
 				pixel_i 	<= '0';
 				row_i		<= '0';
 				frame_i		<= '0';
+				vbullet_i	<= '0';
+
+				hsync		<= '0';
+				vsync		<= '0';
 			else
 				cnt_pixel <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_pixel)) + 1, 3));
 
-				if(to_integer(unsigned(cnt_pixel)) = 0) then
+				if(to_integer(unsigned(cnt_pixel)) = 0) then -- rising_edge(pixel)
 					pixel_i <= '1';
+
+					if((to_integer(unsigned(cnt_row)) > 2) and (to_integer(unsigned(cnt_row)) < 19)) then
+						if(to_integer(unsigned(cnt_frame)) > 7) and (to_integer(unsigned(cnt_frame)) < 472) then
+							if(to_integer(unsigned(cnt_x)) = 15) then
+								cnt_x <= (others => '0');
+							else
+								cnt_x <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_x)) + 1, 4));
+							end if;
+						end if;
+					end if;
+
+					if((to_integer(unsigned(cnt_row)) >= 23) and (to_integer(unsigned(cnt_row)) <= 25)) then
+						hsync <= '0'; -- create hsync signal
+					else
+						hsync <= '1';
+					end if;
 
 					cnt_row <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_row)) + 1, 5));
 
-					if(to_integer(unsigned(cnt_row)) = 0) then
+					if(to_integer(unsigned(cnt_row)) = 0) then -- rising_edge(row)
 						row_i <= '1';
 
 						cnt_frame <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_frame)) + 1, 10));
 
+						if((to_integer(unsigned(cnt_frame)) >= 490) and (to_integer(unsigned(cnt_frame)) <= 491)) then
+							vsync <= '0'; -- create vsync signal
+						else
+							vsync <= '1';
+						end if;
+		
 						if(to_integer(unsigned(cnt_frame)) = 7) then
 							frame_i <= '0';
 						elsif(to_integer(unsigned(cnt_frame)) = 524) then
 							cnt_frame <= (others => '0');
-						elsif(to_integer(unsigned(cnt_frame)) = 471) then
-							frame_i	<= '1';
+						elsif(to_integer(unsigned(cnt_frame)) = 471) then -- rising_edge(frame)
+							frame_i <= '1';
+
+							vbullet_i <= not(vbullet_i);
+						end if;
+		
+						if((to_integer(unsigned(cnt_frame)) > 7) and (to_integer(unsigned(cnt_frame)) < 472)) then
+							cnt_yblck <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_yblck)) + 1, 5));
+		
+							if(to_integer(unsigned(cnt_yblck)) = 0) then  -- create an y signal that shows when to go to the next row in the frame
+								if(to_integer(unsigned(cnt_y)) = 15) then
+									cnt_y <= (others => '0');
+								else
+									cnt_y <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_y)) + 1, 4));
+								end if;
+							elsif(to_integer(unsigned(cnt_yblck)) = 28) then
+								cnt_yblck <= (others => '0');
+							end if;
+						else
+							cnt_yblck <= (others => '0');
 						end if;
 					elsif(to_integer(unsigned(cnt_row)) = 27) then
 						cnt_row <= (others => '0');
@@ -186,6 +232,6 @@ begin
 	frame 	<= frame_i;
 	vbullet <= vbullet_i;
 	pixel 	<= pixel_i;
-	x_c		<= row_i;
-	y_c		<= y_i;
+	x		<= cnt_x;
+	y		<= cnt_y;
 end arch;
--- video-output/timebase_tb.vhd
+++ video-output/timebase_tb.vhd
@@ -8,12 +8,14 @@ end timebase_tb;
 architecture arch of timebase_tb is
 	component timebase is
 		port(
-			clk, reset:						in  std_logic;
-			pixel, frame, vbullet:		out std_logic;
-			hsync, vsync, x_c, y_c: 		out std_logic);
+			clk, reset:				in  std_logic;
+			pixel, frame, vbullet:	out std_logic;
+			hsync, vsync: 			out std_logic;
+			x, y:					out std_logic_vector(3 downto 0));
 	end component timebase;
 
-	signal clk, reset, pixel, frame, vbullet, hsync, vsync, x_c, y_c: std_logic;
+	signal clk, reset, pixel, frame, vbullet, hsync, vsync: std_logic;
+	signal x, y: std_logic_vector(3 downto 0);
 
 begin
 	reset <= 	'1' after 0 	ns,
@@ -23,5 +25,5 @@ begin
 	clk   <= 	'0' after 0 	ns,
 				'1' after 82 	ns when clk /= '1' else '0' after 82 ns;
 
-	l1: timebase port map(clk, reset, pixel, frame, vbullet, hsync, vsync, x_c, y_c);
+	l1: timebase port map(clk, reset, pixel, frame, vbullet, hsync, vsync, x, y);
 end arch;
