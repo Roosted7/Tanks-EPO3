afc9d0e71112d3cf193c111ec106e1c75cb1636c
tijs.moree <t.moree@student.tudelft.nl>
2016-12-05 12:07:15 +0100

    nieuw patroon

--- video-output/vga_test.vhd
+++ video-output/vga_test.vhd
@@ -10,24 +10,35 @@ end vga_test;
 
 architecture arch of vga_test is
 begin
+--	process(x, y)
+--	begin
+--		if(y = "0111" or y = "1000") then
+--			rgb <= "111";
+--		elsif(x = "0010") then
+--			rgb <= "100";
+--		elsif(x = "0011") then
+--			rgb <= "010";
+--		elsif(x = "0100") then
+--			rgb <= "001";
+--		elsif(x = "1010") then
+--			rgb <= "110";
+--		elsif(x = "1011") then
+--			rgb <= "011";
+--		elsif(x = "1100") then
+--			rgb <= "101";
+--		else
+--			rgb <= "000";
+--		end if;
+--	end process;
+
 	process(x, y)
 	begin
-		if(y = "0111" or y = "1000") then
-			rgb <= "111";
-		elsif(x = "0010") then
-			rgb <= "100";
-		elsif(x = "0011") then
-			rgb <= "010";
-		elsif(x = "0100") then
-			rgb <= "001";
-		elsif(x = "1010") then
-			rgb <= "110";
-		elsif(x = "1011") then
-			rgb <= "011";
-		elsif(x = "1100") then
+		if(x = "0010" or x = "1101" or y = "0010" or "1101") then
 			rgb <= "101";
+		elsif((x = "0111" or x = "1000") and (y = "0111" or y = "1000")) then
+			rgb <= "100";
 		else
-			rgb <= "000";
+			rgb <= "001";
 		end if;
 	end process;
 end arch;
