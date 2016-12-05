1ec93866db206d3d0e8e1b3d7af27b078aa38dc6
tijs.moree <t.moree@student.tudelft.nl>
2016-12-05 16:09:56 +0100

    tanks picture added

--- video-output/vga_test.vhd
+++ video-output/vga_test.vhd
@@ -10,7 +10,7 @@ end vga_test;
 
 architecture arch of vga_test is
 begin
-	process(x, y) --vo
+	process(x, y) -- tudelft
 	begin
 		if((x = "0001" and y = "1110") or
 		(x = "0010" and y = "1101") or
@@ -92,4 +92,87 @@ begin
 			rgb <= "111";
 		end if;
 	end process;
+
+--	process(x, y) -- tanks
+--	begin
+--		if(x = "0000" and y = "0000") then
+--			rgb <= "100";
+--		elsif(x = "0000" and y = "0001") then
+--			rgb <= "100";
+--		elsif(x = "0001" and y = "0001") then
+--			rgb <= "100";
+--		elsif(x = "0001" and y = "0010") then
+--			rgb <= "100";
+--		elsif(x = "0001" and y = "0110") then
+--			rgb <= "101";
+--		elsif(x = "0010" and y = "0000") then
+--			rgb <= "100";
+--		elsif(x = "0010" and y = "0001") then
+--			rgb <= "100";
+--		elsif(x = "0011" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "0011" and y = "0100") then
+--			rgb <= "000";
+--		elsif(x = "0011" and y = "0101") then
+--			rgb <= "000";
+--		elsif(x = "0011" and y = "1010") then
+--			rgb <= "000";
+--		elsif(x = "0011" and y = "1011") then
+--			rgb <= "000";
+--		elsif(x = "0011" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "0100" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "0100" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "0101" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "0101" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "0111" and y = "0111") then
+--			rgb <= "000";
+--		elsif(x = "0111" and y = "1000") then
+--			rgb <= "000";
+--		elsif(x = "1000" and y = "0111") then
+--			rgb <= "000";
+--		elsif(x = "1000" and y = "1000") then
+--			rgb <= "000";
+--		elsif(x = "1010" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "1010" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "1011" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "1011" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "0011") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "0100") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "0101") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "1010") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "1011") then
+--			rgb <= "000";
+--		elsif(x = "1100" and y = "1100") then
+--			rgb <= "000";
+--		elsif(x = "1101" and y = "1110") then
+--			rgb <= "001";
+--		elsif(x = "1101" and y = "1111") then
+--			rgb <= "001";
+--		elsif(x = "1110" and y = "1010") then
+--			rgb <= "011";
+--		elsif(x = "1110" and y = "1101") then
+--			rgb <= "001";
+--		elsif(x = "1110" and y = "1110") then
+--			rgb <= "001";
+--		elsif(x = "1111" and y = "1110") then
+--			rgb <= "001";
+--		elsif(x = "1111" and y = "1111") then
+--			rgb <= "001";
+--		else
+--			rgb <= "111";
+--		end if;
+--	end process;
 end arch;
