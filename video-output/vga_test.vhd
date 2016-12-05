7324d01761b517991f523c9e65739e2c21867712
tijs.moree <t.moree@student.tudelft.nl>
2016-12-05 14:52:16 +0100

    removed unused test image

--- video-output/vga_test.vhd
+++ video-output/vga_test.vhd
@@ -10,27 +10,6 @@ end vga_test;
 
 architecture arch of vga_test is
 begin
---	process(x, y)
---	begin
---		if(y = "0111" or y = "1000") then
---			rgb <= "111";
---		elsif(x = "0010") then
---			rgb <= "100";
---		elsif(x = "0011") then
---			rgb <= "010";
---		elsif(x = "0100") then
---			rgb <= "001";
---		elsif(x = "1010") then
---			rgb <= "110";
---		elsif(x = "1011") then
---			rgb <= "011";
---		elsif(x = "1100") then
---			rgb <= "101";
---		else
---			rgb <= "000";
---		end if;
---	end process;
-
 	process(x, y) --vo
 	begin
 		if((x = "0001" and y = "1110") or
