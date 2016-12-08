018a9fb052f9463ccb084a9780efb4254bb9a024
tijs.moree <t.moree@student.tudelft.nl>
2016-12-08 09:04:01 +0100

    removed outcomments

--- video-output/vga_out.vhd
+++ video-output/vga_out.vhd
@@ -7,7 +7,6 @@ entity vga_out is
 		clk, reset:			in  std_logic;
 		rgb: 				in  std_logic_vector(2 downto 0);
 		r, g, b:			out std_logic;
-		--pixel, row:		out std_logic;
 		frame, vbullet:		out std_logic;
 		hsync, vsync: 		out std_logic;
 		x, y:				out std_logic_vector(3 downto 0));
@@ -21,8 +20,6 @@ architecture arch of vga_out is
 	
 	signal cnt_x, cnt_y:	std_logic_vector(3 downto 0);
 
-	--signal pixel_i:			std_logic;
-	--signal row_i:			std_logic;
 	signal frame_i:			std_logic;
 	signal vbullet_i:		std_logic;
 
@@ -31,15 +28,13 @@ begin
 	begin
 		if(rising_edge(clk)) then
 			if(reset = '0') then
-				cnt_pixel 	<= (others => '0'); --vo
+				cnt_pixel 	<= (others => '0');
 				cnt_row 	<= (others => '0');
 				cnt_frame 	<= (others => '0');
 
 				cnt_x 		<= (others => '0');
 				cnt_y 		<= (others => '0');
 
-				--pixel_i 	<= '0';
-				--row_i		<= '0';
 				frame_i		<= '0';
 				vbullet_i	<= '0';
 
@@ -53,8 +48,6 @@ begin
 				cnt_pixel <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_pixel)) + 1, 3));
 
 				if(to_integer(unsigned(cnt_pixel)) = 0) then -- rising_edge(pixel)
-					--pixel_i <= '1';
-
 					if((to_integer(unsigned(cnt_row)) > 2) and (to_integer(unsigned(cnt_row)) < 19) and
 					to_integer(unsigned(cnt_frame)) > 8) and (to_integer(unsigned(cnt_frame)) < 473) then
 						if(to_integer(unsigned(cnt_x)) = 15) then
@@ -78,19 +71,9 @@ begin
 						hsync <= '1';
 					end if;
 
---					if(to_integer(unsigned(cnt_row)) = 0) then
---						hsync <= '1';
---					elsif(to_integer(unsigned(cnt_row)) = 23) then
---						hsync <= '0';
---					elsif(to_integer(unsigned(cnt_row)) = 26) then
---						hsync <= '1';
---					end if;						
-
 					cnt_row <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_row)) + 1, 5));
 
 					if(to_integer(unsigned(cnt_row)) = 0) then -- rising_edge(row)
-						--row_i <= '1';
-
 						cnt_frame <= std_logic_vector(to_unsigned(to_integer(unsigned(cnt_frame)) + 1, 10));
 
 						if((to_integer(unsigned(cnt_frame)) >= 490) and (to_integer(unsigned(cnt_frame)) <= 491)) then
@@ -127,13 +110,9 @@ begin
 						end if;
 					elsif(to_integer(unsigned(cnt_row)) = 27) then
 						cnt_row <= (others => '0');
-					elsif(to_integer(unsigned(cnt_row)) = 14) then
-						--row_i	<= '0';
 					end if;
 				elsif(to_integer(unsigned(cnt_pixel)) = 6) then
 					cnt_pixel <= (others => '0');
-				elsif(to_integer(unsigned(cnt_pixel)) = 4) then
-					--pixel_i	<= '0';
 				end if;
 			end if;
 		end if;
@@ -141,8 +120,6 @@ begin
 
 	frame 	<= frame_i;
 	vbullet <= vbullet_i;
-	--pixel 	<= pixel_i;
 	x		<= cnt_x;
 	y		<= cnt_y;
-	--row 	<= row_i;
 end arch;
