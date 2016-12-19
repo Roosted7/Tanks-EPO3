915cc0010c97e7eb4e735bfba251ffd0e92f9633
aptschipper <aptschipper@tud208928.ws.tudelft.net>
2016-12-19 12:12:27 +0100

    wc

--- pixelgenerator/VHDL files/tb_beh_top_1.vhd
+++
@@ -1,8 +0,0 @@
-configuration tb_beh_top_1 of tb_pg_top is
-   for beh_top
-      for all: pg_top use configuration work.pg_player_1;
-      end for;
-   end for;
-end tb_beh_top_1;
-
-
--- pixelgenerator/VHDL files/tb_ext_pxc.vhd
+++
@@ -1,8 +0,0 @@
-configuration tb_ext_pxc of tb_pg_pxc is
-   for beh_pxc
-      for all: pg_pxc use configuration work.cfg_ext_pg_pxc;
-      end for;
-   end for;
-end tb_ext_pxc;
-
-
