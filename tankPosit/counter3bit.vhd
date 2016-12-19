8c0abe66a320a4408f9454f840bd7ceaea2c7350
jtams <jor.bentvelsen@gmail.com>
2016-12-19 11:53:45 +0100

    all tanKpos added

---
+++ tankPosit/counter3bit.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity counter3bit is
+    Port ( CLK : in  STD_LOGIC;
+           Count : out  STD_LOGIC_VECTOR (2 downto 0));
+end counter3bit;
+
+
+
+
