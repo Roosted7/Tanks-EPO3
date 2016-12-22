96bc11e53e8e5318ba990726845aa342d98782a5
Nuriel Rozsa <nrozsa@ce-epo3.ewi.tudelft.nl>
2016-12-22 11:14:35 +0100

    Copy bestandje

---
+++ copy.sh
@@ -0,0 +1,9 @@
+cd ~/Tanks-EPO3
+git pull
+
+cp ~/Tanks-EPO3/BulletManagement/*.vhd ~/game_top/VHDL
+cp ~/Tanks-EPO3/input/*.vhd ~/game_top/VHDL
+cp ~/Tanks-EPO3/pixelgenerator/VHDL\ files/*.vhd ~/game_top/VHDL
+cp ~/Tanks-EPO3/tankPosition/*.vhd ~/game_top/VHDL
+cp ~/Tanks-EPO3/video-output/*.vhd ~/game_top/VHDL
+cp ~/Tanks-EPO3/pixelgenerator/VHDL\ files/cfgs/*.vhd ~/game_top/VHDL
