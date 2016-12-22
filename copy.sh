d6243350ea65dcc1eeff1b78a5792f61b9c10a5a
Thomas Roos <thomas@GLaDOS>
2016-12-22 11:28:26 +0100

    Geen TBers in dit mapje!

--- copy.sh
+++ copy.sh
@@ -7,3 +7,5 @@ cp ~/Tanks-EPO3/pixelgenerator/VHDL\ files/*.vhd ~/game_top/VHDL
 cp ~/Tanks-EPO3/tankPosition/*.vhd ~/game_top/VHDL
 cp ~/Tanks-EPO3/video-output/*.vhd ~/game_top/VHDL
 cp ~/Tanks-EPO3/pixelgenerator/VHDL\ files/cfgs/*.vhd ~/game_top/VHDL
+
+rm *tb*
