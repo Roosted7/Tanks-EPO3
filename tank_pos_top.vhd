c15bda4dc35cd056e29ecbab97b831dc3cb50f18
jbee2 <jor.bentvelsen@gmail.com>
2017-01-12 18:19:33 +0100

    Added a reset at the top level
    Has to be connected!

---
+++ tank_pos_top.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity tank_pos_top_level is
+   port(input_direction_1	:in    std_logic_vector(2 downto 0);
+        input_direction_2	:in    std_logic_vector(2 downto 0);
+        old_pos_1			  :in    std_logic_vector(10 downto 0);
+        new_pos_1			  :out   std_logic_vector(9 downto 0);
+        old_pos_2			  :in    std_logic_vector(10 downto 0);
+        new_pos_2			  :out   std_logic_vector(9 downto 0);
+        count_in      			 :in    std_logic_vector(2 downto 0);
+		reset		: in std_logic;
+		clk 		: in std_logic);
+end tank_pos_top_level;
+
+
+
+
+
+
+
+
+
+
+
