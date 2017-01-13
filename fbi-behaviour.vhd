79d9c4092c98ecc3a849f87fa458db5378b70d23
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2017-01-13 11:18:58 +0100

    added fbi (fire bullet input) files
    connect infbi to output fire signal of input encoder module van nuriel
    connect outfbi to input fire signal van bullet management module
    connect bclk to bullet clock (30Hz) (zelfde die ook naar bullet management module gaat!)

---
+++ fbi-behaviour.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+architecture behaviour of fbi is
+
+component mem
+   port(d    :in    std_logic;
+        q    :out   std_logic_vector(2 downto 0);
+        reset:in    std_logic;
+        bclk :in    std_logic;
+        clk  :in    std_logic);
+end component;
+
+component s
+   port(inp :in    std_logic_vector(2 downto 0);
+        outp:out   std_logic);
+end component;
+
+component delay
+port(	inp	:in std_logic;
+	outp	:out std_logic;
+	clk	:in std_logic;
+	reset	:in std_logic);
+end component;
+
+signal inp_f, bclk_d : std_logic;
+
+begin
+mem1: mem port map(			inp_f,
+			outfbi,
+			reset,
+			bclk_d,
+			clk);
+
+s1: s port map(			infbi,
+			inp_f);
+
+del: delay port map(			bclk,
+			bclk_d,
+			clk,
+			reset);
+
+			
+end behaviour;
+
+
+
+
+
