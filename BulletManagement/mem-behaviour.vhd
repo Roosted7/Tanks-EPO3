21041e69deffccedd0c36ad073891edc432c3fd8
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2017-01-15 13:54:10 +0100

    fixed rising edge errors

--- BulletManagement/mem-behaviour.vhd
+++ BulletManagement/mem-behaviour.vhd
@@ -2,24 +2,30 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of mem is
+signal pre_q : std_logic_vector(2 downto 0);
+signal r_bclk: std_logic;
+
 begin
-process (clk, bclk) is
+process (clk) is
 begin
-	if(rising_edge(bclk)) then
-		q<="000";
-	elsif(rising_edge(clk)) then
-		if(reset = '1') then
-			q<="000";
-		elsif(q/="101") then
-			q(0)<=d;
-			q(1)<=not(d);
-			q(2)<=d;
+	if(rising_edge(clk)) then
+		if(reset = '1' or r_bclk='1') then
+			pre_q<="000";
+		elsif(pre_q/="101") then
+			pre_q(0)<=d;
+			pre_q(1)<=not(d);
+			pre_q(2)<=d;
 		end if;
 	end if;
+
+	q<=pre_q;
+end process;
+
+process (bclk, clk) is
+begin
+	r_bclk <= bclk;
 end process;
 end behaviour;
-
-
 
 
 
