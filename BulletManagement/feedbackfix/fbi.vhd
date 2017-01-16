31eb03c39da11567b732f3f64249fa9690d3d9e7
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2017-01-16 10:56:34 +0100

    Update fbi.vhd

--- BulletManagement/feedbackfix/fbi.vhd
+++ BulletManagement/feedbackfix/fbi.vhd
@@ -1,11 +1,11 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
-entity fbi is
+entity fbit is
    port(infbi  :in    std_logic;
         bclk :in    std_logic;
         clk  :in    std_logic;
         reset:in    std_logic;
         outfbi :out   std_logic);
-end fbi;
+end fbit;
 
