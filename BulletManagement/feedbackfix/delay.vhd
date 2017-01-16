15825d1eaa24e160dde963e468077315fe72fdf5
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2017-01-16 10:58:06 +0100

    Update delay.vhd

--- BulletManagement/feedbackfix/delay.vhd
+++ BulletManagement/feedbackfix/delay.vhd
@@ -1,10 +1,10 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
-entity delay is
+entity delayt is
 port(	inp	: in std_logic;
 	outp	: out std_logic;
 	clk	: in std_logic;
 	reset	: in std_logic);
-end delay;
+end delayt;
 
